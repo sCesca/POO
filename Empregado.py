340b807c1141992277a70cac1ffef5ce0bf4dcc4
Samuel Fagundes Cesca <samuel1cesca@gmail.com>
2023-09-12 17:53:41 -0300

    Update Empregado.py

--- Empregado.py
+++ Empregado.py
@@ -5,4 +5,30 @@ class Empregado:
     self.rg = rg
 
   def pagamento(self):
-    print()
+    print("O pagamento é um momento de grande felicidade para o funcionáro!")
+    return 0
+
+class EmpregadoHorista(Empregado):
+  def __init__(self, nome, cpf, rg, horasTrabalhadas, pagamentoPorHora):
+    Empregado.__init__(self, nome, cpf, rg)
+    self.horasTrabalhadas = horasTrabalhadas
+    self.pagamentoPorHora = pagamentoPorHora
+
+  def pagamento(self):
+    return self.horasTrabalhadas = self.pagamentoPorHora
+
+class EmpregadoCLT(Empregado):
+  def __init__(self, nome, cpf, rg, salario):
+    Empregado.__init__(self, nome, cpf, rg)
+    self.salario = salario
+
+  def pagamento(self):
+    return 13.33333333 * self.salario
+
+class PrestadorDeServico(Empregado):
+  def __init(self, nome, cpf, rg, pagamentoAvulso):
+    Empregado.__init__(self, nome, cpf, rg)
+    self.pagamentoAvulso = pagamentoAvulso
+
+  def pagamento(self):
+    return self.pagamentoAvulso
